bc9ce08dd245abe508156713d33bfc16551b037d
mtalbi <malik.talbi@ens-lyon.fr[core]>
2022-04-14 13:03:39 +0200

    Prise en compte de la présence dans une region de répétée

--- script/VCF_parser.py
+++ script/VCF_parser.py
@@ -19,20 +19,23 @@ vcf_in=open(sys.argv[1], mode="r")
 vcf_out=open(sys.argv[2], mode="w")
 
 for line in vcf_in.readlines():
+
     if '#' in line:
         vcf_out.write(line) #Add the '#' part of a VCF file
 
     else:
 
-        if 'ANN' in line: #If Ann is in the line and the variant is in masked region with parse it
-            line=line.split('\t') #Parse the line
+        line=line.split('\t') #Parse the line
+
+        if 'ANN' in line[7] and line[10]=='1\n': #If Ann is in the line and the variant is in masked region with parse it
+            
             info=line[7].split(';')#Parse the info part of vcf file
             keep_str=line #Variable useful to rebuild the correct line in the file
             
             for ann in info:
 
                 if 'ANN' in ann: #We get the ANN part 
-                    ann=ann.split('=') #Separtae ANN= and the content of ANN
+                    ann=ann.split('=') #Separate ANN= and the content of ANN
                     keep_str[7]=ann
                     ann=ann[1].split(',') #Parse the different annotation
                     keep_str[7][1]=[]
@@ -53,5 +56,10 @@ for line in vcf_in.readlines():
 
             keep_str[7][1]=','.join(keep_str[7][1]) #We rebuild the line
             keep_str[7]='='.join(keep_str[7])
+            del(keep_str[-1])
             keep_str='\t'.join(keep_str)
-            vcf_out.write(keep_str)
+            vcf_out.write(keep_str+'\n')
+
+        else:
+            del(line[-1])
+            vcf_out.write('\t'.join(line)+'\n')
